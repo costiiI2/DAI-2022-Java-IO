9248e9c5f7e9c564e9cb71bed7a8d28f167d10ab
costi <costantino.cecchet@heig-vd.ch>
2022-10-06 17:22:45 +0200

    NoOp et Upper

--- LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/transformers/UpperCaseCharTransformer.java
+++ LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/transformers/UpperCaseCharTransformer.java
@@ -11,9 +11,7 @@ import java.util.logging.Logger;
 public class UpperCaseCharTransformer {
   private static final Logger LOG = Logger.getLogger(UpperCaseCharTransformer.class.getName());
 
-  public String transform(String c) {
-    /* TODO: implement the transformation here.
-     */
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+  public String transform(String s) {
+   return s.toUpperCase();
   }
 }
