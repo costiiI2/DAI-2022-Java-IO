7192a5c41a5bcfce4c58895d37fda2e3baf4c54a
costiiI2 <71190744+costiiI2@users.noreply.github.com>
2022-10-10 09:54:59 +0200

    exeptions handling

--- LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/FileTransformer.java
+++ LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/FileTransformer.java
@@ -21,28 +21,35 @@ import java.util.logging.Logger;
 public class FileTransformer {
     private static final Logger LOG = Logger.getLogger(FileTransformer.class.getName());
 
-    @SneakyThrows
+
     public void transform(File inputFile) {
         String outName = inputFile + ".out";
         StringBuilder StringToBeWritten = new StringBuilder();
-        //NoOpCharTransformer t = new NoOpCharTransformer();
         UpperCaseCharTransformer upT = new UpperCaseCharTransformer();
         LineNumberingCharTransformer liT = new LineNumberingCharTransformer();
-        Reader reader = new InputStreamReader(new FileInputStream(inputFile), StandardCharsets.UTF_8);
-        int tmp;
-        while ((tmp = reader.read()) != -1) {
-            String charToBeTransformed = "";
-            charToBeTransformed += (char) tmp;
-            charToBeTransformed = upT.transform(charToBeTransformed);
-            StringToBeWritten.append(liT.transform(charToBeTransformed));
-        }
-        reader.close();
+        Reader reader = null;
         try {
-            File outFile = new File(outName);
-            FileOutputStream out = new FileOutputStream(outFile);
+            reader = new InputStreamReader(new FileInputStream(inputFile), StandardCharsets.UTF_8);
+            int tmp;
+            while ((tmp = reader.read()) != -1) {
+                String charToBeTransformed = "";
+                charToBeTransformed += (char) tmp;
+                charToBeTransformed = upT.transform(charToBeTransformed);
+                StringToBeWritten.append(liT.transform(charToBeTransformed));
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            LOG.log(Level.SEVERE, "Error file not found.", e);
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, "Error :", e);
+            throw new RuntimeException(e);
+        }
+
+        File outFile = new File(outName);
+        try (FileOutputStream out = new FileOutputStream(outFile)) {
             byte[] bytes = StringToBeWritten.toString().getBytes(StandardCharsets.UTF_8);
             out.write(bytes);
-            out.close();
         } catch (Exception ex) {
             LOG.log(Level.SEVERE, "Error while reading, writing or transforming file.", ex);
         }
